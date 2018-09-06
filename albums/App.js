4f4c55dbc30e7030147a1bda465589efc050e5f0
Joshua Wood <jwood14@apu.edu>
2018-09-05 18:49:51 -0700

    Finished RN1: 7

--- albums/App.js
+++ albums/App.js
@@ -1,10 +1,15 @@
 // Import a library to create a component
 import React from 'react';
-import Header from './src/components/header';
+import { View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 // Create a component
 const App = () => (
-    <Header headerText={'Albums'} />
+    <View>
+        <Header headerText={'Albums'} />
+        <AlbumList />
+    </View>
 );
 
 // Export it so it is accessible by the index
