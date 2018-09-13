a39ecb52dbd73a9581fa842556c583b206055e9d
Joshua Wood <jwood14@apu.edu>
2018-09-12 19:09:25 -0700

    Almost done with 10, not done with lecture 76

--- auth/App.js
+++ auth/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './src/components/common/index';
+import { Header, Button, Spinner } from './src/components/common/index';
 import LoginForm from './src/components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
         apiKey: 'AIzaSyA11J8090m0HeBUnI5_ys55cgbO4bIK2mQ',
@@ -14,6 +16,26 @@ class App extends Component {
         storageBucket: 'auth-42204.appspot.com',
         messagingSenderId: '433548234975'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      console.log(user);
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return <Button>Log Out</Button>;
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
   }
 
   render() {
