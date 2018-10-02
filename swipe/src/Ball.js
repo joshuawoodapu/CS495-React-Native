9cb7b841f0ba6e65718053d15c1a282276b29947
Joshua Wood <jwood14@apu.edu>
2018-10-01 20:48:15 -0700

    Added a space

--- swipe/src/Ball.js
+++ swipe/src/Ball.js
@@ -3,7 +3,7 @@ import { View, Animated } from 'react-native';
 
 class Ball extends Component {
     componentWillMount() {
-        this.position = new Animated.ValueXY(0,0);
+        this.position = new Animated.ValueXY(0, 0);
         Animated.spring(this.position, {
             toValue: { x: 200, y: 500 }
         }).start();
