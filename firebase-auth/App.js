946fa562a8dd3dece64f7b0c14dc649ae727c8a9
Joshua Wood <jwood14@apu.edu>
2018-10-16 02:23:30 -0700

    Finished section 9

--- firebase-auth/App.js
+++ firebase-auth/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from 'firebase';
 import SignUpForm from './components/SignUpForm';
 import SignInForm from './components/SignInForm';
 
 export default class App extends React.Component {
+  componentDidMount() {
+    const config = {
+      apiKey: "AIzaSyDkvHfUre_Wnfab0bU4aA7UeGjeldxXCSs",
+      authDomain: "one-time-password-3f614.firebaseapp.com",
+      databaseURL: "https://one-time-password-3f614.firebaseio.com",
+      projectId: "one-time-password-3f614",
+      storageBucket: "one-time-password-3f614.appspot.com",
+      messagingSenderId: "917387441052"  
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <View style={styles.container}>
